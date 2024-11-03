73272efb5e50cb10c30b558cc43d079553cc691d
999Ali999 <999alishafiei999@gmail.com>
2024-11-03 07:21:01 +0100

    the backend and frontend core are done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,21 @@
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import HomePage from "./pages/HomePage.jsx";
+
 function App() {
-  return <>test</>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "../assets/styles/bootstrap.custom.css";
+import "../assets/styles/index.css";
 import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import ProductPage from "./pages/ProductPage.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomePage />} />
+      <Route path="/product/:id" element={<ProductPage />} />
+    </Route>,
+  ),
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
