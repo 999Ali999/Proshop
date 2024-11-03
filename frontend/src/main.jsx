c56e71bcc9dd50c45e79812f2e76b5bbc6f5a9db
999Ali999 <999alishafiei999@gmail.com>
2024-11-03 09:26:08 +0100

    cooking the redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,6 +12,8 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
+import { Provider } from "react-redux";
+import store from "./store.js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,6 +26,8 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 );
